88e043e89e1ac5253bc475531de6c706139d98ad
AndemusGBJ <andemus017@outlook.fr>
2019-06-18 13:45:42 +0200

    Création fichier albert.py

---
+++ Albert.py
@@ -0,0 +1,6 @@
+def copy(filedepart, pathdest):
+        
+        # formatage de la commande dos pour la copie
+        
+    command = "copy " + filedepart + " " + pathdest # ommand
+    system(command) # execution de la commande
