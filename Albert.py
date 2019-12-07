2d62c0198bfd8b0beb713535fb5e69007430ab4a
Ana5Famous <hertytambwe@gmail.com>
2019-12-07 10:30:55 +0100

    Albert j'ai fais un ajout d'un print

--- Albert.py
+++ Albert.py
@@ -5,3 +5,5 @@ def copy(filedepart, pathdest):
     command = "copy " + filedepart + " " + pathdest # ommand
     system(command) # execution de la commande
     a = 12
+    #j'ajoute
+    print("je fais un ajout")
